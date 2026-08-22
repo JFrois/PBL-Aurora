38dbb0eb5eda07ad1dd8a22e11cbc4d04a49c813
JFrois <juan.frois9@gmail.com>
2026-08-22 16:24:35 -0300

    Add initial implementation of interactive menu and data handling functions

---
+++ codigo/codigo_fonte.py
@@ -0,0 +1,4 @@
+# Criar menu interativo
+# Criar funções para alocação dos dados registros_colonia.txt (usando open, read, write, append)
+# Criar funções de carregar e salvar dados no dados_colonia.json
+# Criar a versão inicial e válida dos arquivos registros_colonia.txt e dados_colonia.json com dados fictícios.
